0a2569ca1892f857d7f3ffc7e0726221e73c6796
Bandaru98 <bandarumadhavi681998@gmail.com>
2024-09-10 17:27:35 +0530

    update

--- Javascript/Practice/greaternumfind.js
+++
@@ -1,9 +0,0 @@
-function int(array) {
-    const num = array.filter((i, index) => {
-        return i > 10
-    })
-    return num
-    // return num.length
-}
-console.log(int([10, 3, 60, 38, 26, 16, 3, 9, 29]));
-
